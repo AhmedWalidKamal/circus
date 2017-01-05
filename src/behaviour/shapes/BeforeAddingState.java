6d2a96c09717bc6ed945c93286e152dac6824abe
Salma-Ahmed <salma19956@gmail.com>
2017-01-05 23:00:16 +0200

    BeforeAdding state handle

--- src/behaviour/shapes/BeforeAddingState.java
+++ src/behaviour/shapes/BeforeAddingState.java
@@ -1,13 +1,18 @@
 package behaviour.shapes;
 
 import controller.MainController;
+import javafx.animation.PathTransition;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.util.Duration;
 import model.Shelf;
 import model.shapes.Shape;
 
 class BeforeAddingState implements ShapeState {
 
     private MainController mainController = null;
-
+    private Path path = new Path();
     protected BeforeAddingState(final MainController mainController) {
         this.mainController = mainController;
     }
@@ -18,8 +23,24 @@ class BeforeAddingState implements ShapeState {
         double y = shelf.getY();
         switch (shelf.getOrientation()) {
             case LEFT:
+                //Note : change fixed dimension later.
+                path.getElements().add(new MoveTo(350,shelf.getImageView().getFitWidth()));
+                PathTransition leftTransition = new PathTransition();
+                leftTransition.setDuration(Duration.seconds(8));
+                leftTransition.setPath(path);
+                leftTransition.setNode(shape.getImageView());
+                leftTransition.play();
+                shape.getImageView().setX(shelf.getImageView().getFitWidth());
                 break;
             case RIGHT:
+                //Note : change fixed dimension later.
+                path.getElements().add(new MoveTo(1280 - 350 , -shelf.getImageView().getFitWidth()));
+                PathTransition rightTransition = new PathTransition();
+                rightTransition.setDuration(Duration.seconds(8));
+                rightTransition.setPath(path);
+                rightTransition.setNode(shape.getImageView());
+                rightTransition.play();
+                shape.getImageView().setX(shelf.getImageView().getFitWidth());
                 break;
             default:
                 break;
