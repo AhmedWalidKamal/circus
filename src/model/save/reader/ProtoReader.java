164d478aa20fb35b23c1c5be0ecfbf12267afaf4
Ahmed Walid <ahmed3walid96@gmail.com>
2017-01-27 08:48:07 +0200

    Modified Proto Reader.

--- src/model/save/reader/ProtoReader.java
+++ src/model/save/reader/ProtoReader.java
@@ -33,6 +33,8 @@ public class ProtoReader implements Reader {
 
     }
 
+    private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm";
+
 	@Override
 	public ModelMemento loadMemento(final String path)
 			throws FileNotFoundException, IOException {
@@ -45,66 +47,11 @@ public class ProtoReader implements Reader {
 	private ModelMemento createModelMemento(final ProtoGame protoGame) {
     	loadDefaultClasses();
 		ModelMemento loadedMemento = new ModelMemento();
-		loadedMemento.setTimer(new Timer(protoGame.getCurrentTime()));
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-		loadedMemento.setLocalDateTime(LocalDateTime.parse(protoGame.getLocalDateTime(), formatter));
-		loadedMemento.setDifficultyLevel(protoGame.getLevelDifficulty());
+		setMementoAttributes(loadedMemento, protoGame);
 		List<Player> players = new ArrayList<>();
 		for (ProtoPlayer protoPlayer : protoGame.getPlayersList()) {
 			Player player = new Player();
-			player.setName(protoPlayer.getPlayerName());
-			player.setScore(new Score(protoPlayer.getPlayerScore()));
-			Character character
-			= CharacterFactory.getInstance().
-			createCharacter(protoPlayer.
-					getPlayerCharacter().getKey());
-			character.setHeight(protoPlayer.getPlayerCharacter().getHeight());
-			character.setWidth(protoPlayer.getPlayerCharacter().getWidth());
-			character.setLeftStackXInset(protoPlayer.getPlayerCharacter().getLeftStackXInset());
-			character.setLeftStackYInset(protoPlayer.getPlayerCharacter().getLeftStackYInset());
-			character.setRightStackXInset(protoPlayer.getPlayerCharacter().getRightStackXInset());
-			character.setRightStackYInset(protoPlayer.getPlayerCharacter().getRightStackYInset());
-			character.setxProperty(protoPlayer.getPlayerCharacter().getXProperty());
-			character.setyProperty(protoPlayer.getPlayerCharacter().getYProperty());
-			character.setTranslateXProperty(protoPlayer.getPlayerCharacter().getTranslateXProperty());
-			character.setTranslateYProperty(protoPlayer.getPlayerCharacter().getTranslateYProperty());
-			character.setUrl(protoPlayer.getPlayerCharacter().getUrl());
-			character.setKey(protoPlayer.getPlayerCharacter().getKey());
-			Stack<Shape> leftStack = new Stack<>();
-			Stack<Shape> rightStack = new Stack<>();
-			for (ProtoShape protoShape : protoPlayer.getPlayerCharacter().getLeftStackList()) {
-				Shape shape = ShapeFactory.getInstance().
-						createShape(protoShape.getKey(), getColor(protoShape.getColor()));
-				shape.setColor(getColor(protoShape.getColor()));
-				shape.setHeight(protoShape.getHeight());
-				shape.setkey(protoShape.getKey());
-				shape.setState(getState(protoShape.getState()));
-				shape.setUrl(protoShape.getUrl());
-				shape.setWidth(protoShape.getWidth());
-				shape.setX(protoShape.getX());
-				shape.setY(protoShape.getY());
-				shape.setTranslateXProperty(protoShape.getTranslateX());
-				shape.setTranslateYProperty(protoShape.getTranslateY());
-				leftStack.push(shape);
-			}
-			for (ProtoShape protoShape : protoPlayer.getPlayerCharacter().getRightStackList()) {
-				Shape shape = ShapeFactory.getInstance().
-						createShape(protoShape.getKey(), getColor(protoShape.getColor()));
-				shape.setColor(getColor(protoShape.getColor()));
-				shape.setHeight(protoShape.getHeight());
-				shape.setkey(protoShape.getKey());
-				shape.setState(getState(protoShape.getState()));
-				shape.setUrl(protoShape.getUrl());
-				shape.setWidth(protoShape.getWidth());
-				shape.setX(protoShape.getX());
-				shape.setY(protoShape.getY());
-				shape.setTranslateXProperty(protoShape.getTranslateX());
-				shape.setTranslateYProperty(protoShape.getTranslateY());
-				rightStack.push(shape);
-			}
-			character.setLeftStack(leftStack);
-			character.setRightStack(rightStack);
-			player.setCharacter(character);
+			setPlayerAttributes(player, protoPlayer);
 			players.add(player);
 		}
 		loadedMemento.setPlayers(players);
@@ -120,6 +67,68 @@ public class ProtoReader implements Reader {
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private void setMementoAttributes(final ModelMemento loadedMemento, final ProtoGame protoGame) {
+		loadedMemento.setTimer(new Timer(protoGame.getCurrentTime()));
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+		loadedMemento.setLocalDateTime(LocalDateTime.parse(protoGame.getLocalDateTime(), formatter));
+		loadedMemento.setDifficultyLevel(protoGame.getLevelDifficulty());
+	}
+
+	private void setPlayerAttributes(final Player player, final ProtoPlayer protoPlayer) {
+		player.setName(protoPlayer.getPlayerName());
+		player.setScore(new Score(protoPlayer.getPlayerScore()));
+		Character character
+		= CharacterFactory.getInstance().
+		createCharacter(protoPlayer.
+				getPlayerCharacter().getKey());
+		setCharacterAttributes(character, protoPlayer);
+		player.setCharacter(character);
+	}
+
+	private void setCharacterAttributes(final Character character, final ProtoPlayer protoPlayer) {
+		character.setHeight(protoPlayer.getPlayerCharacter().getHeight());
+		character.setWidth(protoPlayer.getPlayerCharacter().getWidth());
+		character.setLeftStackXInset(protoPlayer.getPlayerCharacter().getLeftStackXInset());
+		character.setLeftStackYInset(protoPlayer.getPlayerCharacter().getLeftStackYInset());
+		character.setRightStackXInset(protoPlayer.getPlayerCharacter().getRightStackXInset());
+		character.setRightStackYInset(protoPlayer.getPlayerCharacter().getRightStackYInset());
+		character.setxProperty(protoPlayer.getPlayerCharacter().getXProperty());
+		character.setyProperty(protoPlayer.getPlayerCharacter().getYProperty());
+		character.setTranslateXProperty(protoPlayer.getPlayerCharacter().getTranslateXProperty());
+		character.setTranslateYProperty(protoPlayer.getPlayerCharacter().getTranslateYProperty());
+		character.setUrl(protoPlayer.getPlayerCharacter().getUrl());
+		character.setKey(protoPlayer.getPlayerCharacter().getKey());
+		Stack<Shape> leftStack = new Stack<>();
+		Stack<Shape> rightStack = new Stack<>();
+		for (ProtoShape protoShape : protoPlayer.getPlayerCharacter().getLeftStackList()) {
+			Shape shape = ShapeFactory.getInstance().
+					createShape(protoShape.getKey(), getColor(protoShape.getColor()));
+			setShapeAttributes(shape, protoShape);
+			leftStack.push(shape);
+		}
+		for (ProtoShape protoShape : protoPlayer.getPlayerCharacter().getRightStackList()) {
+			Shape shape = ShapeFactory.getInstance().
+					createShape(protoShape.getKey(), getColor(protoShape.getColor()));
+			setShapeAttributes(shape, protoShape);
+			rightStack.push(shape);
+		}
+		character.setLeftStack(leftStack);
+		character.setRightStack(rightStack);
+	}
+
+	private void setShapeAttributes(final Shape shape, final ProtoShape protoShape) {
+		shape.setColor(getColor(protoShape.getColor()));
+		shape.setHeight(protoShape.getHeight());
+		shape.setkey(protoShape.getKey());
+		shape.setState(getState(protoShape.getState()));
+		shape.setUrl(protoShape.getUrl());
+		shape.setWidth(protoShape.getWidth());
+		shape.setX(protoShape.getX());
+		shape.setY(protoShape.getY());
+		shape.setTranslateXProperty(protoShape.getTranslateX());
+		shape.setTranslateYProperty(protoShape.getTranslateY());
 	}
 
 	private State getState(final String state) {
