b5e4a3396b83988ae1706877dd8e2d2332076f8d
Ahmed Walid <ahmed3walid96@gmail.com>
2017-01-27 08:32:26 +0200

    Modified Proto Writer.

--- src/model/save/writer/ProtoWriter.java
+++ src/model/save/writer/ProtoWriter.java
@@ -9,12 +9,14 @@ import model.Player;
 import model.save.ModelMemento;
 import model.save.util.ProtoBuffGame.ProtoGame;
 import model.save.util.ProtoBuffGame.ProtoGame.ProtoPlayer;
+import model.save.util.ProtoBuffGame.ProtoGame.ProtoPlayer.Builder;
 import model.save.util.ProtoBuffGame.ProtoGame.ProtoPlayer.ProtoCharacter;
 import model.save.util.ProtoBuffGame.ProtoGame.ProtoPlayer.ProtoCharacter.ProtoShape;
 import model.shapes.Shape;
 
 public class ProtoWriter implements Writer {
 
+	private static final String DATE_FORMAT = "yyyy-MM-dd HH:mm";
 	public ProtoWriter() {
 
 	}
@@ -34,59 +36,84 @@ public class ProtoWriter implements Writer {
 
 	private ProtoGame createProtoGame(final ModelMemento memento) {
 		ProtoGame.Builder protoGameBuilder = ProtoGame.newBuilder();
-		protoGameBuilder.setCurrentTime(memento.getTimer().getCurrentTime());
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-		protoGameBuilder.setLocalDateTime(memento.getLocalDateTime().format(formatter));
-		protoGameBuilder.setLevelDifficulty(memento.getDifficultyLevel());
+		setProtoGameAttributes(protoGameBuilder, memento);
 		for (Player player : memento.getPlayers()) {
 			ProtoPlayer.Builder protoPlayerBuilder = ProtoPlayer.newBuilder();
-			protoPlayerBuilder.setPlayerName(player.getName());
-			protoPlayerBuilder.setPlayerScore(player.getScore().getCurrentScore());
-			ProtoCharacter.Builder protoCharBuilder = ProtoCharacter.newBuilder();
-			protoCharBuilder.setWidth(player.getCharacter().getWidth());
-			protoCharBuilder.setHeight(player.getCharacter().getHeight());
-			protoCharBuilder.setLeftStackXInset(player.getCharacter().getLeftStackXInset());
-			protoCharBuilder.setLeftStackYInset(player.getCharacter().getLeftStackYInset());
-			protoCharBuilder.setRightStackXInset(player.getCharacter().getRightStackXInset());
-			protoCharBuilder.setRightStackYInset(player.getCharacter().getRightStackYInset());
-			protoCharBuilder.setXProperty(player.getCharacter().getxProperty().doubleValue());
-			protoCharBuilder.setYProperty(player.getCharacter().getyProperty().doubleValue());
-			protoCharBuilder.setTranslateXProperty(player.getCharacter().getTranslateXProperty().doubleValue());
-			protoCharBuilder.setTranslateYProperty(player.getCharacter().getTranslateYProperty().doubleValue());
-			protoCharBuilder.setUrl(player.getCharacter().getUrl());
-			protoCharBuilder.setKey(player.getCharacter().getKey());
-			for (Shape shape : player.getCharacter().getLeftStack()) {
-				ProtoShape.Builder protoShapeBuilder = ProtoShape.newBuilder();
-				protoShapeBuilder.setState(shape.getState().toString());
-				protoShapeBuilder.setColor(shape.getColor().toString());
-				protoShapeBuilder.setUrl(shape.getUrl());
-				protoShapeBuilder.setWidth(shape.getWidth());
-				protoShapeBuilder.setHeight(shape.getHeight());
-				protoShapeBuilder.setX(shape.getX());
-				protoShapeBuilder.setY(shape.getY());
-				protoShapeBuilder.setTranslateX(shape.getTranslateX().doubleValue());
-				protoShapeBuilder.setTranslateY(shape.getTranslateY().doubleValue());
-				protoShapeBuilder.setKey(shape.getKey());
-				protoCharBuilder.addLeftStack(protoShapeBuilder);
-			}
-			for (Shape shape : player.getCharacter().getRightStack()) {
-				ProtoShape.Builder protoShapeBuilder = ProtoShape.newBuilder();
-				protoShapeBuilder.setState(shape.getState().toString());
-				protoShapeBuilder.setColor(shape.getColor().toString());
-				protoShapeBuilder.setUrl(shape.getUrl());
-				protoShapeBuilder.setWidth(shape.getWidth());
-				protoShapeBuilder.setHeight(shape.getHeight());
-				protoShapeBuilder.setX(shape.getX());
-				protoShapeBuilder.setY(shape.getY());
-				protoShapeBuilder.setTranslateX(shape.getTranslateX().doubleValue());
-				protoShapeBuilder.setTranslateY(shape.getTranslateY().doubleValue());
-				protoShapeBuilder.setKey(shape.getKey());
-				protoCharBuilder.addRightStack(protoShapeBuilder);
-			}
-			protoPlayerBuilder.setPlayerCharacter(protoCharBuilder);
+			setProtoPlayerAttributes(protoPlayerBuilder, player);
 			protoGameBuilder.addPlayers(protoPlayerBuilder);
 		}
 		return protoGameBuilder.build();
 	}
 
+	private void setProtoGameAttributes(final ProtoGame.Builder protoGameBuilder,
+			final ModelMemento memento) {
+		protoGameBuilder.setCurrentTime(memento.
+				getTimer().getCurrentTime());
+		DateTimeFormatter formatter = DateTimeFormatter.
+				ofPattern(DATE_FORMAT);
+		protoGameBuilder.setLocalDateTime(memento.
+				getLocalDateTime().format(formatter));
+		protoGameBuilder.setLevelDifficulty(memento.
+				getDifficultyLevel());
+	}
+
+	private void setProtoPlayerAttributes(final Builder protoPlayerBuilder, final Player player) {
+		protoPlayerBuilder.setPlayerName(player.getName());
+		protoPlayerBuilder.setPlayerScore(player.
+				getScore().getCurrentScore());
+		ProtoCharacter.Builder protoCharBuilder = ProtoCharacter.newBuilder();
+		setProtoCharacterAttributes(protoCharBuilder, player);
+		protoPlayerBuilder.setPlayerCharacter(protoCharBuilder);
+	}
+
+	private void setProtoCharacterAttributes(
+			final ProtoCharacter.Builder protoCharBuilder,
+			final Player player) {
+		protoCharBuilder.setWidth(player.getCharacter().getWidth());
+		protoCharBuilder.setHeight(player.getCharacter().getHeight());
+		protoCharBuilder.setLeftStackXInset(player.
+				getCharacter().getLeftStackXInset());
+		protoCharBuilder.setLeftStackYInset(player.
+				getCharacter().getLeftStackYInset());
+		protoCharBuilder.setRightStackXInset(player.
+				getCharacter().getRightStackXInset());
+		protoCharBuilder.setRightStackYInset(player.
+				getCharacter().getRightStackYInset());
+		protoCharBuilder.setXProperty(player.
+				getCharacter().getxProperty().doubleValue());
+		protoCharBuilder.setYProperty(player.
+				getCharacter().getyProperty().doubleValue());
+		protoCharBuilder.setTranslateXProperty(player.
+				getCharacter().getTranslateXProperty().doubleValue());
+		protoCharBuilder.setTranslateYProperty(player.
+				getCharacter().getTranslateYProperty().doubleValue());
+		protoCharBuilder.setUrl(player.getCharacter().getUrl());
+		protoCharBuilder.setKey(player.getCharacter().getKey());
+		for (Shape shape : player.getCharacter().getLeftStack()) {
+			ProtoShape.Builder protoShapeBuilder = ProtoShape.newBuilder();
+			setProtoShapeAttributes(protoShapeBuilder, shape);
+			protoCharBuilder.addLeftStack(protoShapeBuilder);
+		}
+		for (Shape shape : player.getCharacter().getRightStack()) {
+			ProtoShape.Builder protoShapeBuilder = ProtoShape.newBuilder();
+			setProtoShapeAttributes(protoShapeBuilder, shape);
+			protoCharBuilder.addRightStack(protoShapeBuilder);
+		}
+	}
+
+	private void setProtoShapeAttributes(final ProtoShape.Builder protoShapeBuilder,
+			final Shape shape) {
+		protoShapeBuilder.setState(shape.getState().toString());
+		protoShapeBuilder.setColor(shape.getColor().toString());
+		protoShapeBuilder.setUrl(shape.getUrl());
+		protoShapeBuilder.setWidth(shape.getWidth());
+		protoShapeBuilder.setHeight(shape.getHeight());
+		protoShapeBuilder.setX(shape.getX());
+		protoShapeBuilder.setY(shape.getY());
+		protoShapeBuilder.setTranslateX(shape.
+				getTranslateX().doubleValue());
+		protoShapeBuilder.setTranslateY(shape.
+				getTranslateY().doubleValue());
+		protoShapeBuilder.setKey(shape.getKey());
+	}
 }
